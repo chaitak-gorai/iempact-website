17d820b7cb7d25b3b8a0933e153d378d4d119b3f
Arijit Das <arijit10das2002@gmail.com>
2022-11-06 17:32:53 +0530

    events

--- components/Events/Events.jsx
+++ components/Events/Events.jsx
@@ -1,29 +1,58 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import IEMPACT from "../../public/images/IEMPACT Logo.png";
 
-const Events = () => {
+const Events = (event) => {
   return (
     <>
-    <div className="container">
-    <div id='content'>
-    <div className="item e5" >
-        <Image 
-        src={IEMPACT}
-        className="img-fluid "
-        alt=""
-        />
-        <div id="e5bk"></div>
-        <div className="text5">
-            <h3>Boating & Fishing</h3>
-            <p>Drop a Line in the River Laune.</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <div id="content">
+              <div className="item e5">
+                <Image src={IEMPACT} className="img-fluid " alt="" />
+                <div id="e5bk"></div>
+                <div className="text5">
+                  <h3>Boating & Fishing</h3>
+                  <p>Drop a Line in the River Laune.</p>
+                </div>
+                <div className="line inside"></div>
+              </div>
+            </div>
+            
+          </div>
+          <div className="col-lg-4">
+            <div id="content">
+              <div className="item e5">
+                <Image src={IEMPACT} className="img-fluid " alt="" />
+                <div id="e5bk"></div>
+                <div className="text5">
+                  <h3>Boating & Fishing</h3>
+                  <p>Drop a Line in the River Laune.</p>
+                </div>
+                <div className="line inside"></div>
+              </div>
+            </div>
+            
+          </div>
+          <div className="col-lg-4">
+            <div id="content">
+              <div className="item e5">
+                <Image src={IEMPACT} className="img-fluid " alt="" />
+                <div id="e5bk"></div>
+                <div className="text5">
+                  <h3>Boating & Fishing</h3>
+                  <p>Drop a Line in the River Laune.</p>
+                </div>
+                <div className="line inside"></div>
+              </div>
+            </div>
+            
+          </div>
         </div>
-        <div className="line inside"></div>
-    </div>
-    </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Events;
