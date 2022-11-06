add35424945543ebdc946fa669e11cd7e815233b
Pritam Das <pritamstudy123@gmail.com>
2022-11-06 17:38:46 +0530

    dates

---
+++ components/Dates/Dates.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import  Countdown from './Lund.jsx'
+
+export default function Dates() {
+ 
+  return (
+    <section id="call-to-action" className="features">
+    <div className="container" data-aos="fade-up">
+      <header className="section-header">
+        <h3>Important Dates</h3>
+        
+      </header>
+
+      <div className="row">
+       
+
+        <div className="">
+          <div className="row align-self-center gy-4">
+            <div className="col-md-12" data-aos="zoom-out" data-aos-delay="200">
+              <div className="feature-box align-items-center text-center">
+                <h3>OCTOBER</h3>
+                <h3 >&nbsp;14th, 15th & 16th</h3>
+                <div className="App">
+                     <Countdown/>
+                  </div>
+              </div>
+            </div>
+
+
+
+
+          
+          </div>
+        </div>
+      </div>
+   
+    </div>
+  </section>
+  )
+}
