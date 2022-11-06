e3ec08f85d4e10af8279bf38d8e9afdda34ec37b
Pritam Das <pritamstudy123@gmail.com>
2022-11-06 16:38:50 +0530

    register button

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,64 +1,66 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Link from "next/link";
+import IEMlogo from "../../public/logo/IEM_Logo.png";
+import Image from "next/image";
 
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Link from 'next/link'
-import IEMlogo from '../../public/logo/IEM_Logo.png'
-import Image from 'next/image'
-
-import React from 'react'
+import React from "react";
 
 const TopNavbar = () => {
   return (
     <Navbar
-    collapseOnSelect
-    expand='lg'
-    className='navabar'
-    variant='light'
-    fixed='top'
-  >
-    <Container>
-      <Link href='/'>
-        <Navbar.Brand>
-          <Image
-            alt=''
-            src={IEMlogo}
-            height='60'
-            className='d-inline-block align-top'
-          />
-          <img
-            alt=''
-            src={''}
-            height='60'
-            margin='0px  10px'
-            className='d-inline-block align-top logo'
-          />
-          <img
-            alt=''
-            src={''}
-            height='60'
-            margin='0px  10px'
-            className='d-inline-block align-top logo'
-          />
-        </Navbar.Brand>
-      </Link>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='me-auto'></Nav>
-        <Nav style={{fontWeight:'700',}}>
-          <Nav.Link href='#hero' >Home</Nav.Link>
-          <Nav.Link href='#about' >Discover More</Nav.Link>
-          <Nav.Link href='#partners'>Our Sponsors</Nav.Link>
-          <Nav.Link href='/'>Rule Book</Nav.Link>
-          
-          {/* <Nav.Link href='#contact'>Contact</Nav.Link> */}
-        </Nav>
+      collapseOnSelect
+      expand="lg"
+      className="navabar"
+      variant="light"
+      fixed="top"
+    >
+      <Container>
+        <Link href="/">
+          <Navbar.Brand>
+            <Image
+              alt=""
+              src={IEMlogo}
+              height="60"
+              className="d-inline-block align-top"
+            />
+            <img
+              alt=""
+              src={""}
+              height="60"
+              margin="0px  10px"
+              className="d-inline-block align-top logo"
+            />
+            <img
+              alt=""
+              src={""}
+              height="60"
+              margin="0px  10px"
+              className="d-inline-block align-top logo"
+            />
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav style={{ fontWeight: "700" }}>
+            <Nav.Link href="#hero">Home</Nav.Link>
+            <Nav.Link href="#about">Discover More</Nav.Link>
+            <Nav.Link href="#partners">Our Sponsors</Nav.Link>
+            <Nav.Link href="/">Rule Book</Nav.Link>
+            <Nav>
+              <a className="btn-get-started scrollto animate__animated animate__fadeInUp">
+                Register Now
+              </a>
+            </Nav>
+            {/* <Nav.Link href='#contact'>Contact</Nav.Link> */}
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
