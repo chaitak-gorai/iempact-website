78420b268f89922fb9a6c514625bb9b5fe340cd6
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2022-11-07 14:34:06 +0530

    added google analytics, changed favicon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,20 +12,34 @@ import Dates from '../components/Dates/Dates'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>IEMPACT</title>
-        <meta name='description' content='IEMPACT, the Annual Cultural Fest of IEM, is back with its 32nd edition after 2 years of online lives- education, events. As the Annual Cultural Fest of IEM, Saltlake we bring to you dance, music, football, quiz, debate, art and most importantly Hope and Joy.' />
-        <link rel='icon' href='/favicon.ico' />
-        <link href="https://fonts.googleapis.com/css2?family=Changa:wght@300;400&display=swap" rel="stylesheet"></link>
-      </Head>
+    <>
+      <Script id="GA" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-JJ8DY46ZZ5`} />
+      <Script id="GA" strategy="lazyOnload">
+                  {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-JJ8DY46ZZ5', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+      </Script>
 
-      <Navbar />
-      <Hero />
-      <About />
-      <Dates/>
-      <Events/>
-      <Footer/>
-    </div>
+      <div className={styles.container}>
+        <Head>
+          <title>IEMPACT 2k23</title>
+          <meta name='description' content='IEMPACT, the Annual Cultural Fest of IEM, is back with its 32nd edition after 2 years of online lives- education, events. As the Annual Cultural Fest of IEM, Saltlake we bring to you dance, music, football, quiz, debate, art and most importantly Hope and Joy.' />
+          <link rel='icon' href='/images/IEMPACT.png' />
+          <link href="https://fonts.googleapis.com/css2?family=Changa:wght@300;400&display=swap" rel="stylesheet"></link>
+        </Head>
+
+        <Navbar />
+        <Hero />
+        <About />
+        <Dates/>
+        <Events/>
+        <Footer/>
+      </div>
+    </>
   )
 }
