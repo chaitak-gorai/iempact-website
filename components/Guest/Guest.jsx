84ee6c57f3911c74b53c6205f5a0d5574c4eb33b
Pritam Das <pritamstudy123@gmail.com>
2022-12-15 03:19:14 +0530

    ...

--- components/Guest/Guest.jsx
+++ components/Guest/Guest.jsx
@@ -10,40 +10,6 @@ const Guest = () => {
         <header className="section-header">
           <h3>Guest Performance</h3>
         </header>
-        {/* <div className="row py-5">
-          <div className="col-lg-6 img">
-            <Image
-              src={Trap}
-              className="img-fluid "
-              alt=""
-              data-aos="fade-right" data-aos-duration="2500"
-            />
-          </div>
-          <div className="col-lg-6 write" >
-            <div className="music1">
-              <Image
-                src={Tabla}
-                className="img-fluid "
-                alt=""
-                data-aos="zoom-in" data-aos-duration="2500"
-              />
-            </div>
-            <h1>Pandit Bickram Ghosh </h1>
-            <h2>21st January</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-          </div>
-        </div> */}
         <div className="row pt-5">
           <div className="col-lg-6 order-first order-lg-last img">
             <Image
